95b664b6210aea155185e925d51045e12b6c0b00
Michael Ugochukwu <mark.omulu@gmail.com>
2022-05-19 14:37:11 +0100

    Add comment API for psoting and getting comments

---
+++ src/modules/comments-handler.js
@@ -0,0 +1,26 @@
+// @ts-ignore
+const URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const appID = 'HqxG6qkpi3DhZGaAkW1w';
+const endPoint = `${URL}/apps/${appID}/comments/`;
+
+const postComment = async (buttonId, userName, userComment) => {
+  const res = await fetch(endPoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: buttonId,
+      username: userName.value,
+      comment: userComment.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; Charset=UTF-8',
+    },
+  });
+  return res.text();
+};
+
+const getComments = async (id) => {
+  const response = await fetch(`${endPoint}?item_id=${id}`);
+  return response.json();
+};
+
+export { postComment, getComments };
